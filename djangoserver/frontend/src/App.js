2b42a73e10912538300a9fcef229c4b7099eadb9
benja789 <molina.benjamin70@gmail.com>
2021-04-15 10:22:54 -0600

    Añadir imagen

--- djangoserver/frontend/src/App.js
+++ djangoserver/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
+import image from './media/react-logo.png';
 import './App.css';
 import Contador from './Components/Contador';
 
@@ -10,6 +11,7 @@ function App() {
       <title>Titulo de react</title>
       <header className="App-header">
         <img src={"/static/dist/" + logo} className="App-logo" alt="logo" />
+        <img src={ "/static/dist/"+image} />
         <h1>Esta pagina esta hecha mediante react y siendo renderizada por medio de un servidor de Django</h1>
         <Contador/>
       </header>
